01edce542c361354e13cc70a7090f1c43a965c24
Matheus Eli F <matheuselisouzaferreira@hotmail.com>
2017-11-27 21:52:24 -0200

    Mudanças Arquivos

--- Práticas_Programação/Aula_8_PP/Aula_8_Arquivos/src/aula_8_arquivos/Aula_8_Arquivos.java
+++ Práticas_Programação/Aula_8_PP/Aula_8_Arquivos/src/aula_8_arquivos/Aula_8_Arquivos.java
@@ -3,19 +3,67 @@ package aula_8_arquivos;
 import java.io.*;
 public class Aula_8_Arquivos {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws IOException { 
         
-        String entrada = "12345678";
+        File arquivo = new File("teste.bin");
         
-        FileOutputStream is = new FileOutputStream("arquivo.txt");
-        
-        for (int i = 0; i < entrada.length(); i++) {
+        try( OutputStream os = new FileOutputStream(arquivo) ){ 
             
-            is.write(entrada.charAt(i));
-        }
-        
-        System.out.println("Dados gravados: "+entrada);
-        is.close();
-    }
+            byte[] b = {50,51,52,53};
+            String string = "Anhembi Morumbi";
+            os.write(53);
+            os.write(b);
+            os.write( string.getBytes() );
+            os.flush();
     
+        }catch(IOException ex){
+        
+            ex.printStackTrace(); }
+        
+        try( InputStream is = new FileInputStream(arquivo) ){ 
+            int content;
+            
+            while ( (content = is.read() ) != -1) {
+                System.out.println( content +" - "+ ( (char) content) );
+            
+            }}catch(IOException ex){
+                ex.printStackTrace();
+            }
+        
+        
+        
+        
+            File arquivo1 = new File("teste.txt"); 
+        
+            try( FileWriter fw = new FileWriter(arquivo1) ){ 
+            
+                fw.write('2'); 
+            
+                fw.write("25"); 
+        
+                fw.flush();// força a gravação do buffer no arquivo 
+            }catch(IOException ex){ 
+                
+                ex.printStackTrace(); 
+            }
+            
+            
+            
+            File arquivo3 = new File("teste.txt");
+            
+            try(FileReader fr = new FileReader(arquivo3)){
+            
+            int c = fr.read();
+            
+            while(c != -1){
+            
+                System.out.print((char)c);
+                c = fr.read();
+            }}catch(IOException ex){
+            
+                ex.printStackTrace();
+            }
+
+    }
 }
+
